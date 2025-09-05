5d8a67d3660f15554a2d6aee26a738c5d6dce7a6
Young <kimyoh123@gmail.com>
2025-09-05 13:56:01 -0700

    finalize snake game

--- snake_game/main.py
+++ snake_game/main.py
@@ -4,6 +4,7 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 screen = Screen()
@@ -37,18 +38,17 @@ while game_is_on:
     xcor = snake.head.xcor()
     ycor = snake.head.ycor()
     if xcor > (SCREEN_WIDTH / 2 - 20) or xcor < (-SCREEN_WIDTH/2) or ycor > (SCREEN_HEIGHT / 2 - 20) or ycor < (
-            -SCREEN_HEIGHT / 2 + 20):
+            -SCREEN_HEIGHT / 2):
         game_is_on = False
         scoreboard.game_over()
 
 
     # detect collision with tail
     # if head collides with any segment in the tail trigger game_over
-    for segment in snake.segments:
-        if segment  == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
+
 
 screen.exitonclick()
