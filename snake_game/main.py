d602b3929df595401b068b916a42e940a210cd56
Young <kimyoh123@gmail.com>
2025-09-05 16:47:56 -0700

    update snake game to display highscore

--- snake_game/main.py
+++ snake_game/main.py
@@ -39,16 +39,14 @@ while game_is_on:
     ycor = snake.head.ycor()
     if xcor > (SCREEN_WIDTH / 2 - 20) or xcor < (-SCREEN_WIDTH/2) or ycor > (SCREEN_HEIGHT / 2 - 20) or ycor < (
             -SCREEN_HEIGHT / 2):
-        game_is_on = False
-        scoreboard.game_over()
-
+        scoreboard.reset()
+        snake.reset_snake()
 
     # detect collision with tail
     # if head collides with any segment in the tail trigger game_over
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+            scoreboard.reset()
+            snake.reset_snake()
 
 screen.exitonclick()
